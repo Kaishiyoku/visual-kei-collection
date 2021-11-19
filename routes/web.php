f152f57179e2ef1dea28244d71ddf71a7e66239e
Kaishiyoku <dmp@nfze.de>
2021-11-19 16:09:21 +0100

    adjusted user authentication

--- routes/web.php
+++ routes/web.php
@@ -17,6 +17,14 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
-    return view('dashboard');
-})->name('dashboard');
+// auth routes
+Route::group(['middleware' => ['auth:sanctum', 'verified']], function () {
+    Route::get('/dashboard', function () {
+        return view('dashboard');
+    })->name('dashboard');
+
+    // admin routes
+    Route::group(['middleware' => ['administrate']], function () {
+
+    });
+});
