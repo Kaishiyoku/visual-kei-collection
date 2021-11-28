551d7ca18513ad31349fd257b7219a30313c130e
Kaishiyoku <dmp@nfze.de>
2021-11-28 16:16:13 +0100

    disabled autocomplete for source input field

--- resources/views/image/_form_fields.blade.php
+++ resources/views/image/_form_fields.blade.php
@@ -9,7 +9,7 @@
 <div class="mb-4">
     <x-jet-label for="source" :value="__('validation.attributes.source')"/>
 
-    <x-jet-input id="source" class="block mt-1 w-full" type="text" name="source" :value="old('source', $image->source)"/>
+    <x-jet-input id="source" class="block mt-1 w-full" type="text" name="source" :value="old('source', $image->source)" autocomplete="off"/>
 
     <x-validation-error for="source"/>
 </div>
