20f1b21ed08a8ccb75e949236b28ad50a48c3af7
Kaishiyoku <dmp@nfze.de>
2021-12-01 00:07:08 +0100

    updated Composer dependencies

--- app/Models/PossibleDuplicate.php
+++ app/Models/PossibleDuplicate.php
@@ -5,6 +5,28 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * App\Models\PossibleDuplicate
+ *
+ * @property int $id
+ * @property int $image_id_left
+ * @property int $image_id_right
+ * @property int $is_false_positive
+ * @property \Illuminate\Support\Carbon|null $created_at
+ * @property \Illuminate\Support\Carbon|null $updated_at
+ * @property-read \App\Models\Image $imageLeft
+ * @property-read \App\Models\Image $imageRight
+ * @method static \Illuminate\Database\Eloquent\Builder|PossibleDuplicate newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|PossibleDuplicate newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|PossibleDuplicate query()
+ * @method static \Illuminate\Database\Eloquent\Builder|PossibleDuplicate whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|PossibleDuplicate whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|PossibleDuplicate whereImageIdLeft($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|PossibleDuplicate whereImageIdRight($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|PossibleDuplicate whereIsFalsePositive($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|PossibleDuplicate whereUpdatedAt($value)
+ * @mixin \Eloquent
+ */
 class PossibleDuplicate extends Model
 {
     use HasFactory;
